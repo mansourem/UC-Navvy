09416b9042b672414713990649edcd3431504730
Elaine Mansour <mansour.elaine.1@gmail.com>
2025-06-18 16:29:14 -0400

    add networkx import for graph handling

--- temp.py
+++ temp.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 import json
+import networkx as nx
+
+graph = nx.Graph()
 
 with open("nodes.json",'r') as n:
     nodes = json.load(n)
 
-print(nodes)
+for node in nodes["nodes"]:
+    print(node)
+    print("-----------------")
+    graph.add_node(node)
+
+print(list(graph.nodes))
+# print(nodes)
