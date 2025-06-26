3b2f15a936877ec140039b5b05f84a0f93729f0f
LoganCalabrese <logan.calabrese@outlook.com>
2025-06-26 14:48:30 -0400

    added entrance

--- temp.py
+++ temp.py
@@ -14,12 +14,15 @@ G = nx.Graph()
 
 # Add nodes with lat/lon as attributes
 positions = {}
+entrance_nodes = []
 for node in nodes_data:
     node_id = node['node_id']
     lat = node['latitude']
     lon = node['longitude']
     G.add_node(node_id, **node)
     positions[node_id] = (lon, lat)  # (x, y) = (longitude, latitude)
+    if node.get('entrance', False):  # <-- This line adds entrance nodes
+        entrance_nodes.append(node_id)
 
 # Add edges
 for path in edges_data:
@@ -28,13 +31,21 @@ for path in edges_data:
         if not G.has_edge(node, target):  # Avoid duplicate edges
             G.add_edge(node, target)
 
+# Build node color list
+node_colors = []
+for n in G.nodes():
+    if n in entrance_nodes:
+        node_colors.append('silver')  # Entrance nodes
+    else:
+        node_colors.append('indianred')    # Regular nodes
+
 # Draw the graph
 plt.figure(figsize=(12, 8))
 nx.draw(G, 
         pos=positions, 
         with_labels=True, 
         node_size=100, 
-        node_color='indianred', 
+        node_color= node_colors, 
         edge_color='gray', 
         font_size=8
 )
