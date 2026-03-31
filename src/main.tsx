62b168a69a2f255047015625e3d9a15606fa5cd9
Elaine Mansour <mansour.elaine.1@gmail.com>
2026-03-31 17:13:04 -0400

    make not blank deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './css/base.css';
+import './css/layout.css';
+import './css/components.css';
+import './css/map.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(<App />);
